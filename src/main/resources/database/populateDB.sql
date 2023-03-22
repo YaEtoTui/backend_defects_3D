076c33f18df68dec0eebbc33e1139ab48f54c499
Egor <sazhin2003.egor@gmail.com>
2023-03-22 17:00:38 +0500

    Fix /api/defects/{id}

--- src/main/resources/database/populateDB.sql
+++ src/main/resources/database/populateDB.sql
@@ -1,3 +1,3 @@
 INSERT INTO defects VALUES
-(1, 'Первый дефект', 'https://otkritkis.com/wp-content/uploads/2022/01/nastol.com_.ua-41512.jpg'),
-(2, 'Второй дефект', 'Ссылки нет');
+(1, 'Первый дефект', 'paris.jpg'),
+(2, 'Второй дефект', 'Ссылки нет')
